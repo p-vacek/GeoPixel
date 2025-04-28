171fcd6bd3486b2fb239655882346a757a6d9952
p-vacek <vacek@spaceknow.com>
2025-04-28 13:42:38 +0000

    pip package for cog.yaml

--- src/GeoPixel/inference.py
+++ src/GeoPixel/inference.py
@@ -1,4 +1,7 @@
-# %%
+# Example Inference of GeoPixel on image data. Set query and image input and the model will return predictions with response description.
+# The model require at least two 24 VRAM GPUs and takes few minutes to download, initialize and around 30sec inference.
+# SpaceKnow Inc.
+
 import os
 from PIL import Image
 import torch
@@ -8,15 +11,15 @@ import re
 import torch
 import random
 import numpy as np
-import matplotlib.pyplot as plt
-from textwrap import wrap
 
 from model.geopixel import GeoPixelForCausalLM
+
 
 def rgb_color_text(text, r, g, b):
     return f"\033[38;2;{r};{g};{b}m{text}\033[0m"
 
-# setup
+
+# setup Model
 version = "MBZUAI/GeoPixel-7B"
 print(f"initialing tokenizer from: {version}")
 tokenizer = transformers.AutoTokenizer.from_pretrained(
@@ -52,155 +55,71 @@ model.config.bos_token_id = tokenizer.bos_token_id
 model.config.pad_token_id = tokenizer.pad_token_id
 model.tokenizer = tokenizer
 
-# %%
+
 # Inference
-# query = f"Can you provide a thorough description of the this image? Please output with interleaved segmentation masks for the corresponding phrases." 
 query = f"find all planes on the image and output interleaved segmentation masks for the corresponding phrases."
 
 image_path = "images/example1.png"
-img = Image.open(image_path).resize((560,560))
+img = Image.open(image_path).resize((560, 560))
 
 # normalize
-visual_procesor = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.48145466, 0.4578275, 0.40821073),
-                                 (0.26862954, 0.26130258, 0.27577711)),
-        ])
+visual_procesor = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize(
+            (0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)
+        ),
+    ]
+)
 
 img = visual_procesor(img).unsqueeze(0).unsqueeze(0).bfloat16()
 device_type = next(iter(model.parameters())).device.type
 img = img.to(device_type)
 
 response, masks = model.evaluate(tokenizer, query, img, max_new_tokens=1024, hd_num=1)
-# %%
-
-
-
 
 pred_masks = masks
-if pred_masks and '[SEG]' in response:
-
-# if pred_masks:
+if pred_masks and "[SEG]" in response:
+    # if pred_masks:
     pred_masks = pred_masks[0]
     pred_masks = pred_masks.detach().cpu().numpy()
     pred_masks = pred_masks > 0
-    image_np = np.asarray(Image.open(image_path).resize((560,560)))
+    image_np = np.asarray(Image.open(image_path).resize((560, 560)))
     # image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-    
+
     save_img = image_np.copy()
-    pattern = r'<p>(.*?)</p>\s*\[SEG\]'
+    pattern = r"<p>(.*?)</p>\s*\[SEG\]"
     matched_text = re.findall(pattern, response)
     phrases = [text.strip() for text in matched_text]
 
     for i in range(pred_masks.shape[0]):
         mask = pred_masks[i]
-        
+
         color = [random.randint(0, 255) for _ in range(3)]
         if matched_text:
             phrases[i] = rgb_color_text(phrases[i], color[0], color[1], color[2])
-        mask_rgb = np.stack([mask, mask, mask], axis=-1) 
+        mask_rgb = np.stack([mask, mask, mask], axis=-1)
         color_mask = np.array(color, dtype=np.uint8) * mask_rgb
 
-        save_img = np.where(mask_rgb, 
-                (save_img * 0.5 + color_mask * 0.5).astype(np.uint8), 
-                save_img)
-    if matched_text:    
-        split_desc = response.split('[SEG]')
-        cleaned_segments = [re.sub(r'<p>(.*?)</p>', '', part).strip() for part in split_desc]
+        save_img = np.where(
+            mask_rgb, (save_img * 0.5 + color_mask * 0.5).astype(np.uint8), save_img
+        )
+    if matched_text:
+        split_desc = response.split("[SEG]")
+        cleaned_segments = [
+            re.sub(r"<p>(.*?)</p>", "", part).strip() for part in split_desc
+        ]
         reconstructed_desc = ""
         for i, part in enumerate(cleaned_segments):
-            reconstructed_desc += part + ' '
+            reconstructed_desc += part + " "
             if i < len(phrases):
-                reconstructed_desc += phrases[i] + ' '    
+                reconstructed_desc += phrases[i] + " "
         print(reconstructed_desc)
     else:
         print(response.replace("\n", "").replace("  ", " "))
-    
-    # save_img = cv2.cvtColor(save_img, cv2.COLOR_RGB2BGR)
-    save_path = "{}/{}_masked.jpg".format(
-        "vis_output", image_path.split("/")[-1]
-        )
-    # cv2.imwrite(save_path, save_img)
-    # plt.close()
-    # plt.imshow(save_img)
-    # plt.title("\n".join(wrap(response, width=60)))
+
+    save_path = "{}/{}_masked.jpg".format("vis_output", image_path.split("/")[-1])
+
     print("{} has been saved.".format(save_path))
 else:
     print(response.replace("\n", "").replace("  ", " "))
-
-
-# %%
-# transfer to base64 <--> image
-# import base64
-# from PIL import Image
-# from io import BytesIO
-# import numpy as np
-# import subprocess
-# import json
-
-# with open("cog/GeoPixel/output.1.png", "rb") as image_file:
-#     data = base64.b64encode(image_file.read())
-
-# im = Image.open(BytesIO(base64.b64decode(data)))
-# im.save('cog/GeoPixel/outputbase.0.png', 'PNG')
-
-# out_np = base64.b64encode(np.asarray(im))
-# im = Image.open(BytesIO(base64.b64decode(data)))
-# im.save('cog/GeoPixel/outputbase.np.0.png', 'PNG')
-
-# import requests
-
-# # image = Image.open("cog/GeoPixel/output.1.png")
-# image = Image.open("images/airport.jpg")
-
-# import base64
-# from io import BytesIO
-
-# buffered = BytesIO()
-# image.save(buffered, format="JPEG")
-# data = base64.b64encode(buffered.getvalue())
-# # data = base64.b64encode(np.asarray(image))
-
-
-# url = 'http://localhost:8070/predictions'
-# payload = f'{{"input": {{"image": "data:image/png;base64,{data.decode("utf-8")}"}}}}'
-# headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-# results = requests.post(url, data=payload, headers=headers)
-
-
-# parsed_data = results.json()
-
-# img_base64 = parsed_data["output"].split(";base64,", 1)[1]
-
-# im = Image.open(BytesIO(base64.b64decode(img_base64)))
-# im
-
-# # %%
-# # Docker
-# # subprocess.call()
-# # os.system("docker inspect -f '{{.State.Status}}' focused_tesl")
-# import subprocess
-# import time
-# import sys
-# import os
-# import signal # Useful for sending specific signals if needed
-
-# # --- Configuration ---
-# # 1. Define the command to start your server.
-# #    Adjust this based on your specific server application.
-
-# # Example 1: Python's built-in HTTP server
-# # Runs 'python -m http.server 8080'
-# server_command = ['cog', 'serve', '-p','8070']
-
-# process = subprocess.Popen(
-#         server_command,
-#         stdout=subprocess.PIPE, # Capture standard output
-#         stderr=subprocess.PIPE, # Capture standard error
-#         text=True,              # Decode stdout/stderr as text (Python 3.7+)
-#         # On Windows, you might need creationflags for better termination control
-#         # creationflags=subprocess.CREATE_NEW_PROCESS_GROUP, # Example
-#         # On Unix, preexec_fn can be used to set process group ID
-#         # preexec_fn=os.setsid # Example: Makes it session leader, easier to kill group later
-#     )
-# # %%
